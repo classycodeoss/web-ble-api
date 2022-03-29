afd13412aae349ba5e092f8ed9790d7d2fd47b69
Marco Inniger <marco@classycode.com>
2022-03-29 07:34:30 +0200

    Added requesting bluetooth devices

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,10 +3,12 @@
 
 	let isAvailable: boolean = false;
 	let isLoading: boolean;
-	let bluetoothDevices: BluetoothDevice[];
+	let availableBluetoothDevices: BluetoothDevice[];
+	let requestedBluetoothDevice: BluetoothDevice;
 
 	onMount(() => {
 		enableBluetooth();
+		getAccessibleBluetoothDevices();
 	});
 
 	async function enableBluetooth(): Promise<void> {
@@ -21,16 +23,35 @@
 		}
 	}
 
-	async function getBluetoothDevices(): Promise<void> {
+	async function requestBluetoothDevices(): Promise<void> {
 		isLoading = true;
 		try {
-			console.log('looking for bluetooth devices...');
-			bluetoothDevices = await navigator.bluetooth.getDevices();
-			console.log('Bluetooth devices found: ', bluetoothDevices);
+			console.log('Requesting Bluetooth Devices');
+			requestedBluetoothDevice = await navigator.bluetooth.requestDevice({
+				optionalServices: ["battery_service", "device_information"],
+				acceptAllDevices: true
+			});
+			console.log('Found bluetooth device: ', requestedBluetoothDevice);
 			return Promise.resolve();
 		} catch (e) {
 			console.error(e);
-			bluetoothDevices = [];
+			requestedBluetoothDevice = undefined;
+			return Promise.reject();
+		} finally {
+			isLoading = false;
+		}
+	}
+
+	async function getAccessibleBluetoothDevices(): Promise<void> {
+		isLoading = true;
+		try {
+			console.log('looking for bluetooth devices...');
+			availableBluetoothDevices = await navigator.bluetooth.getDevices();
+			console.log('Bluetooth devices found: ', availableBluetoothDevices);
+			return Promise.resolve();
+		} catch (e) {
+			console.error(e);
+			availableBluetoothDevices = [];
 			return Promise.reject();
 		} finally {
 			isLoading = false;
@@ -40,18 +61,14 @@
 
 <div class="container mx-auto">
 	<h1 class="text-3xl mt-4 mb-2">Web Bluetooth API</h1>
-	<button
-		on:click={enableBluetooth}
-		class="bg-orange-600 text-white p-2 rounded-lg hover:bg-orange-700"
-	>
-		Enable Bluetooth API
-	</button>
-	<button
-		on:click={getBluetoothDevices}
-		class="bg-orange-600 text-white p-2 rounded-lg hover:bg-orange-700"
-	>
+	<button on:click={enableBluetooth} class="btn btn-secondary"> Enable Bluetooth API </button>
+	<button on:click={getAccessibleBluetoothDevices} class="btn btn-secondary">
 		Search Bluetooth Devices
 	</button>
+	<button on:click={requestBluetoothDevices} class="btn btn-primary">
+		Request Bluetooth Device
+	</button>
+
 	{#if isAvailable}
 		<div class="bg-green-400 text-white my-4 p-3 rounded-md flex flex-row items-center gap-2">
 			<span class="material-icons"> check_circle </span>
@@ -65,23 +82,28 @@
 			</div>
 			<span class="ml-8">
 				Check out this
-				<a class="text-blue-200 underline" rel="external" href="https://chromestatus.com/feature/5264933985976320">guide</a>
-				, to enable bluetooth (only available for Chrome)
+				<a
+					class="text-blue-200 underline"
+					rel="external"
+					href="https://chromestatus.com/feature/5264933985976320">guide</a
+				>
+				, to enable bluetooth (only available for Chrome, Edge or Opera)
 			</span>
 		</div>
 	{/if}
 
-	{#if bluetoothDevices && bluetoothDevices.length > 0}
-		{#each bluetoothDevices as device}
+	{#if availableBluetoothDevices && availableBluetoothDevices.length > 0}
+		{#each availableBluetoothDevices as device}
 			<div>
 				<span>{device.id}</span>
 				<span>{device.name}</span>
 				<span>{device.gatt}</span>
 			</div>
 		{/each}
-	{:else if bluetoothDevices}
+	{:else if availableBluetoothDevices}
 		<div>
-			<span>No Bluetooth devices found</span>
+			<span class="text-xl">No Available Bluetooth devices found</span> <br>
+			<span class="text-slate-700">Previously connected devices would show up here, if there are any.</span>
 		</div>
 	{:else}
 		<div>
