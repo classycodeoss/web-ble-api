144124e2e6ca9481db3383b5633d7100b7c6defa
Marco Inniger <marco@classycode.com>
2022-04-05 13:38:35 +0200

    Reformatted code with spaces instead of tabs

--- src/lib/components/AccelerometerData.svelte
+++ src/lib/components/AccelerometerData.svelte
@@ -1,82 +1,86 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { Axis } from '$lib/models/accelerator-data.model';
-	import { TransformUtil } from '$lib/util/transform.util';
-	import { CanvasUtil } from '$lib/util/canvas.util';
-	import { Constants } from '$lib/constants/constants';
-	import { UUIDs } from '$lib/constants/ble-uuids';
-	import type { AccelerometerData } from '$lib/models/accelerator-data.model';
+  import { onMount } from 'svelte';
+  import { Axis } from '$lib/models/accelerator-data.model';
+  import { TransformUtil } from '$lib/util/transform.util';
+  import { CanvasUtil } from '$lib/util/canvas.util';
+  import { Constants } from '$lib/constants/constants';
+  import { UUIDs } from '$lib/constants/ble-uuids';
+  import type { AccelerometerData } from '$lib/models/accelerator-data.model';
 
-	export let gattServer: BluetoothRemoteGATTServer;
+  export let gattServer: BluetoothRemoteGATTServer;
 
-	let wrappingDiv: HTMLDivElement;
-	let context: CanvasRenderingContext2D;
+  let wrappingDiv: HTMLDivElement;
+  let context: CanvasRenderingContext2D;
 
-	let data: AccelerometerData[] = [];
+  let data: AccelerometerData[] = [];
 
-	onMount(() => {
-		const newCanvas = document.createElement('canvas');
-		newCanvas.width = Constants.CanvasWidth;
-		newCanvas.height = Constants.CanvasHeight;
-		newCanvas.setAttribute('class', 'bg-gray-200 rounded-lg');
-		wrappingDiv.appendChild(newCanvas);
-		context = newCanvas.getContext('2d');
-		addListeners();
-	});
+  onMount(() => {
+    const newCanvas = document.createElement('canvas');
+    newCanvas.width = Constants.CanvasWidth;
+    newCanvas.height = Constants.CanvasHeight;
+    newCanvas.setAttribute('class', 'bg-gray-200 rounded-lg');
+    wrappingDiv.appendChild(newCanvas);
+    context = newCanvas.getContext('2d');
+    subscribeToAccelerometerCharacteristic();
+  });
 
-	function redrawData(): void {
-		context.clearRect(0, 0, Constants.CanvasWidth, Constants.CanvasHeight);
-		const normalizedDataX = normalizeData(data, Axis.x);
-		context.fillStyle = '#003f5c';
-		context.strokeStyle = '#003f5c';
-		CanvasUtil.drawCanvas(normalizedDataX, context);
-		const normalizedDateY = normalizeData(data, Axis.y);
-		context.fillStyle = '#bc5090';
-		context.strokeStyle = '#bc5090';
-		CanvasUtil.drawCanvas(normalizedDateY, context);
-		const normalizedDataZ = normalizeData(data, Axis.z);
-		context.fillStyle = '#ffa600';
-		context.strokeStyle = '#ffa600';
-		CanvasUtil.drawCanvas(normalizedDataZ, context);
-	}
+  function redrawData(): void {
+    context.clearRect(0, 0, Constants.CanvasWidth, Constants.CanvasHeight);
+    const normalizedDataX = normalizeData(data, Axis.x);
+    context.fillStyle = '#003f5c';
+    context.strokeStyle = '#003f5c';
+    CanvasUtil.drawCanvas(normalizedDataX, context);
+    const normalizedDateY = normalizeData(data, Axis.y);
+    context.fillStyle = '#bc5090';
+    context.strokeStyle = '#bc5090';
+    CanvasUtil.drawCanvas(normalizedDateY, context);
+    const normalizedDataZ = normalizeData(data, Axis.z);
+    context.fillStyle = '#ffa600';
+    context.strokeStyle = '#ffa600';
+    CanvasUtil.drawCanvas(normalizedDataZ, context);
+  }
 
-	async function addListeners(): Promise<void> {
-		try {
-			const accService = await gattServer.getPrimaryService(UUIDs.AccelerometerServiceUUID);
-			const accConfigCharacteristisc = await accService.getCharacteristic(UUIDs.AccelerometerConfigUUID);
-			await accConfigCharacteristisc.writeValue(new Uint8Array([1]));
-			const accPeriodCharacteristic = await accService.getCharacteristic(UUIDs.AccelerometerPeriodUUID);
-			await accPeriodCharacteristic.writeValue(new Uint8Array([Constants.Interval]));
-			const accDataCharacteristic = await accService.getCharacteristic(UUIDs.AccelerometerDataUUID);
-			console.log('Start Accelerometer notifications');
-			accDataCharacteristic.oncharacteristicvaluechanged = onAccelerometerChanged;
-			accDataCharacteristic.startNotifications();
-		} catch (e) {
-			console.error(e);
-		}
-	}
+  async function subscribeToAccelerometerCharacteristic(): Promise<void> {
+    try {
+      const accService = await gattServer.getPrimaryService(UUIDs.AccelerometerServiceUUID);
+      const accConfigCharacteristisc = await accService.getCharacteristic(
+        UUIDs.AccelerometerConfigUUID
+      );
+      await accConfigCharacteristisc.writeValue(new Uint8Array([1]));
+      const accPeriodCharacteristic = await accService.getCharacteristic(
+        UUIDs.AccelerometerPeriodUUID
+      );
+      await accPeriodCharacteristic.writeValue(new Uint8Array([Constants.Interval]));
+      const accDataCharacteristic = await accService.getCharacteristic(UUIDs.AccelerometerDataUUID);
+      console.log('Start Accelerometer notifications');
+      accDataCharacteristic.oncharacteristicvaluechanged = onAccelerometerChanged;
+      accDataCharacteristic.startNotifications();
+    } catch (e) {
+      console.error(e);
+    }
+  }
 
-	function getAccelerometerValues(data) {
-		const ax = data.getInt8(0, true);
-		const ay = data.getInt8(1, true);
-		const az = data.getInt8(2, true);
-		return { x: ax, y: ay, z: az };
-	}
+  function getAccelerometerValues(data) {
+    const ax = data.getInt8(0, true);
+    const ay = data.getInt8(1, true);
+    const az = data.getInt8(2, true);
+    return { x: ax, y: ay, z: az };
+  }
 
-	function onAccelerometerChanged(event) {
-		const characteristic = event.target;
-		const values = getAccelerometerValues(characteristic.value);
-		data = data.slice(-9).concat(values);
-		redrawData();
-	}
+  function onAccelerometerChanged(event) {
+    const characteristic = event.target;
+    const values = getAccelerometerValues(characteristic.value);
+    data = data.slice(-9).concat(values);
+    redrawData();
+  }
 
-	function normalizeData(data: AccelerometerData[], axis: Axis): number[] {
-		const axisData = data.map((point) => point[axis]);
-		return TransformUtil.normalizeData(axisData, -128, 127);
-	}
+  function normalizeData(data: AccelerometerData[], axis: Axis): number[] {
+    const axisData = data.map((point) => point[axis]);
+    return TransformUtil.normalizeData(axisData, -128, 127);
+  }
 </script>
 
 <div class="my-4">
-	<h2 class="text-xl mb-2">Accelerometer Data</h2>
-	<div class="flex flex-row gap-2" bind:this={wrappingDiv} style="height: 564px;" />
+  <h2 class="text-xl mb-2">Accelerometer Data</h2>
+  <div class="flex flex-row gap-2" bind:this={wrappingDiv} style="height: 564px;" />
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,90 +1,59 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import BluetoothAvailableIndicator from '$lib/components/BluetoothAvailableIndicator.svelte';
-	import DeviceInformation from '$lib/components/DeviceInformation.svelte';
-	import AccelerometerData from '$lib/components/AccelerometerData.svelte';
+  import { onMount } from 'svelte';
+  import BluetoothAvailableIndicator from '$lib/components/BluetoothAvailableIndicator.svelte';
+  import DeviceInformation from '$lib/components/DeviceInformation.svelte';
+  import AccelerometerData from '$lib/components/AccelerometerData.svelte';
 
-	let isAvailable: boolean = false;
-	let availableBluetoothDevices: BluetoothDevice[];
-	let deviceServer: BluetoothRemoteGATTServer;
+  let isAvailable: boolean = false;
+  let deviceServer: BluetoothRemoteGATTServer;
+  let connectedDevice: BluetoothDevice;
 
-	let connectedDevice: BluetoothDevice;
+  onMount(() => {
+    enableBluetooth();
+  });
 
-	const accelerometerServiceUUID = 'f000aa10-0451-4000-b000-000000000000';
+  async function enableBluetooth(): Promise<void> {
+    try {
+      isAvailable = await navigator.bluetooth.getAvailability();
+      console.log('Bluetooth is enabled: ', isAvailable);
+      return Promise.resolve();
+    } catch (e) {
+      console.error(e);
+      isAvailable = false;
+      return Promise.reject();
+    }
+  }
 
-	onMount(() => {
-		enableBluetooth();
-	});
-
-	async function enableBluetooth(): Promise<void> {
-		try {
-			isAvailable = await navigator.bluetooth.getAvailability();
-			console.log('Bluetooth is enabled: ', isAvailable);
-			return Promise.resolve();
-		} catch (e) {
-			console.error(e);
-			isAvailable = false;
-			return Promise.reject();
-		}
-	}
-
-	async function requestBluetoothDevices(): Promise<void> {
-		try {
-			console.log('Requesting Bluetooth Devices');
-			const device = await navigator.bluetooth.requestDevice({
-				filters: [{ namePrefix: 'TI' }],
-				optionalServices: ['battery_service', 'device_information', accelerometerServiceUUID]
-			});
-			deviceServer = await device.gatt.connect();
-			connectedDevice = deviceServer.device;
-			return Promise.resolve();
-		} catch (e) {
-			console.error(e);
-			return Promise.reject();
-		}
-	}
-
-	async function getAccessibleBluetoothDevices(): Promise<void> {
-		try {
-			availableBluetoothDevices = await navigator.bluetooth.getDevices();
-			if (availableBluetoothDevices.length >= 1 && availableBluetoothDevices[0]) {
-				deviceServer = await availableBluetoothDevices[0].gatt.connect();
-				if (deviceServer.device) {
-					connectedDevice = deviceServer.device;
-					await connectedDevice.watchAdvertisements();
-					const logFunction = (event: Event) => {
-						console.error(event);
-						connectedDevice = undefined;
-					};
-					connectedDevice.ongattserverdisconnected = logFunction;
-				}
-			}
-			return Promise.resolve();
-		} catch (e) {
-			console.error(e);
-			availableBluetoothDevices = [];
-			return Promise.reject();
-		}
-	}
+  async function requestBluetoothDevices(): Promise<void> {
+    try {
+      console.log('Requesting Bluetooth Devices');
+      const device = await navigator.bluetooth.requestDevice({
+        filters: [{ namePrefix: 'TI' }],
+      });
+      deviceServer = await device.gatt.connect();
+      connectedDevice = deviceServer.device;
+      return Promise.resolve();
+    } catch (e) {
+      console.error(e);
+      return Promise.reject();
+    }
+  }
 </script>
 
 <div class="container mx-auto px-4 sm:px-0">
-	<h1 class="text-3xl mt-4 mb-4">Web Bluetooth API</h1>
+  <h1 class="text-3xl mt-4 mb-4">Web Bluetooth API</h1>
 
-	<div class="flex flex-row gap-3 mb-2">
-		<button on:click={getAccessibleBluetoothDevices} class="btn btn-secondary">
-			Search Bluetooth Devices
-		</button>
-		<button on:click={requestBluetoothDevices} class="btn btn-primary">
-			Request Bluetooth Device
-		</button>
-	</div>
+  <button on:click={requestBluetoothDevices} disabled={!isAvailable} class="btn btn-primary">
+    Connect Bluetooth Device
+  </button>
 
-	<BluetoothAvailableIndicator {isAvailable} />
+  <BluetoothAvailableIndicator {isAvailable} />
 
-	<DeviceInformation device={connectedDevice} />
+  {#if isAvailable}
+    <DeviceInformation device={connectedDevice} />
+  {/if}
 
-	{#if deviceServer}
-		<AccelerometerData gattServer={deviceServer} />
-	{/if}
+  {#if deviceServer}
+    <AccelerometerData gattServer={deviceServer} />
+  {/if}
 </div>
